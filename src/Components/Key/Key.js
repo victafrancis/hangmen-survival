3bd9bbf1f39b2049fb63f03f87143edf7cb34a6f
Aldrin Jacildo <ajacildo1999@gmail.com>
2020-05-06 14:56:55 -0400

    added keyboard and key component

---
+++ src/Components/Key/Key.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Key.css"
+const Key = () => {
+  return <div></div>;
+};
+
+export default Key;
---
+++ src/Components/Keyboard/Keyboard.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Key from "../Key/Key";
+import "./Keyboard.css";
+
+const Keyboard = () => {
+  return <div></div>;
+};
+
+export default Keyboard;
