3273f22914ab00e34847c96e5478c35214c22549
Francis Victa <francisvicta45@gmail.com>
2020-05-06 14:46:22 -0400

    added local game parent component

---
+++ src/Components/LocalMatch/LocalMatch.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import './LocalMatch.css';
+import WOrdGenerator from '../WordGenerator/WordGenerator';
+
+const LocalMatch = (props) => {
+
+
+  return (
+    <>
+      <WOrdGenerator />
+    </>
+  );
+};
+
+export default LocalMatch;
